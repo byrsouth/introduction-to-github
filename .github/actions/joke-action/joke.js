04b77ef28febc0e1438de937e3fbd515a0a62966
Byron <byron.southerland@icloud.com>
2023-01-15 17:58:22 -0500

    Fixed modules.exports

--- .github/actions/joke-action/joke.js
+++ .github/actions/joke-action/joke.js
@@ -7,12 +7,12 @@ const options = {
       Accept: 'application/json',
       'User-Agent': 'Writing JavaScript action GitHub Skills course.',
    },
-   json: true
+   json: true,
 };
 
 async function getJoke() {
-    const res = await request(options);
-    return res.joke;
+   const res = await request(options);
+   return res.joke;
 }
 
-modules.exports = getJoke;
+module.exports = getJoke;
